b7aa574faaf863c259802de1b84fa08e24fc7406
cleggacus <cleggacus@gmail.com>
2024-06-09 18:35:29 +0100

    vecel

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import vercel from "solid-start-vercel";
 
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [
+            vercel({ edge: true })
+        ],
+    }
+});
